f787afc7881146e8e52ec368353b3da7ff711c0e
fanqi <fanqisoft@gmail.com>
2019-04-17 12:13:53 +0800

    添加OAuth Client Demo，修复了部门问题

---
+++ OAuthClientDemo/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OAuthClientDemo.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetInfo()
+        {
+            string userName = HttpContext.User.Identity.Name;
+            string authType = HttpContext.User.Identity.AuthenticationType;
+            var result = await HttpContext.AuthenticateAsync();
+            var token = result.Properties.GetTokens().Select(t => new string[] { t.Name, t.Value });
+
+            Dictionary<string, string> claims = new Dictionary<string, string>();
+            var claimList = HttpContext.User.Claims.ToList();
+            foreach (var item in claimList)
+            {
+                claims.Add(item.Type, item.Value);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync();
+            HttpContext.Response.Redirect("/");
+        }
+    }
+}
---
+++ OAuthClientDemo/Startup.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OAuthClientDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OAuthDefaults.DisplayName;
+            })
+            .AddCookie()
+            .AddOAuth(OAuthDefaults.DisplayName, options =>
+            {
+                options.ClientId = "server.code";
+                options.ClientSecret = "secret";
+                options.AuthorizationEndpoint = "https://demo.identityserver.io/connect/authorize";
+                options.TokenEndpoint = "https://demo.identityserver.io/connect/token";
+                options.CallbackPath = "/signin-oauth";
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.Scope.Add("email");
+                options.Scope.Add("api");
+                options.Scope.Add("offline_access");
+                options.SaveTokens = true;  //是否将OAuth服务器返回的Token信息保存到AuthenticationProperties中。
+
+                // 事件执行顺序 ：
+                // 1.创建Ticket之前触发
+                options.Events.OnCreatingTicket = context => Task.CompletedTask;
+                // 2.创建Ticket失败时触发
+                options.Events.OnRemoteFailure = context => Task.CompletedTask;
+                // 3.Ticket接收完成之后触发
+                options.Events.OnTicketReceived = context => Task.CompletedTask;
+                // 4.Challenge时触发，默认跳转到OAuth服务器
+                // options.Events.OnRedirectToAuthorizationEndpoint = context => context.Response.Redirect(context.RedirectUri);
+            });
+
+
+
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
